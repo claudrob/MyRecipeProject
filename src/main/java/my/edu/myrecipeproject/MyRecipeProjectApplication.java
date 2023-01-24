f1724cfa493ae6fdf15b57ede9751a4f93fe4f5d
Claud Robinson <crobinson4@west.com>
2023-01-24 12:37:32 -0600

    Initial Spring Project

--- src/main/java/my/edu/myrecipeproject/MyRecipeProjectApplication.java
+++ src/main/java/my/edu/myrecipeproject/MyRecipeProjectApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyRecipeProjectApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MyRecipeProjectApplication.class, args);
+        System.out.println("Startup Test!");
     }
 
 }
