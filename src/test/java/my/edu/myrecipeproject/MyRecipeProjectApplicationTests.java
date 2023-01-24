f17c989abbb877d5f05cd5c09cdcb1e67b20f1fc
Claud Robinson <crobinson4@west.com>
2023-01-24 12:32:58 -0600

    Initial Spring Project

---
+++ src/test/java/my/edu/myrecipeproject/MyRecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package my.edu.myrecipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
